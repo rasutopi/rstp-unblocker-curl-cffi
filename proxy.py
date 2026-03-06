24ac8378d110bffc0574c65eb31e9d32928106e7
rasutopi <rasutopi@gmail.com>
2026-03-06 18:35:51 +0900

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import PlainTextResponse
 from curl_cffi import requests
+from fastapi.responses import StreamingResponse
 
 app = FastAPI()
+
 
 @app.middleware("http")
 async def cors_middleware(request: Request, call_next):
@@ -10,6 +12,7 @@ async def cors_middleware(request: Request, call_next):
         response = Response(status_code=204)
     else:
         response = await call_next(request)
+
 
     origin = request.headers.get("origin")
     if origin:
@@ -20,7 +23,10 @@ async def cors_middleware(request: Request, call_next):
             "access-control-request-headers", "*"
         )
 
+
     return response
+
+
 
 
 @app.api_route("/", methods=["GET", "POST"])
@@ -28,18 +34,26 @@ async def proxy(request: Request, url: str = None):
     if not url:
         return PlainTextResponse("Missing url", status_code=400)
 
+
     try:
         # --- クライアントからヘッダー取得 ---
         incoming_cookies = request.headers.get("cookie")
+
 
         headers = {
             "User-Agent": request.headers.get("user-agent"),
             "Accept": request.headers.get("accept"),
             "Accept-Language": request.headers.get("accept-language"),
         }
+        
+        range_header = request.headers.get("range")
+        if range_header:
+            headers["Range"] = range_header
+
 
         if incoming_cookies:
             headers["Cookie"] = incoming_cookies
+
 
         # --- upstreamへリクエスト ---
         r = requests.request(
@@ -49,18 +63,21 @@ async def proxy(request: Request, url: str = None):
             data=await request.body(),
             impersonate="chrome136",
             allow_redirects=False,
-            timeout=20,
+            timeout=120,
+            stream=True
         )
 
         # --- Content-Type を必ず取得 ---
         content_type = r.headers.get("content-type", "application/octet-stream")
 
+
         # --- レスポンス作成 ---
-        response = Response(
-            content=r.content,
+        response = StreamingResponse(
+            r.iter_content(chunk_size=8192),
             status_code=r.status_code,
             media_type=content_type
         )
+
 
         # --- 危険ヘッダー除外して転送 ---
         excluded_headers = {
@@ -70,11 +87,12 @@ async def proxy(request: Request, url: str = None):
             "connection"
         }
 
+
         for key, value in r.headers.items():
             if key.lower() in excluded_headers:
                 continue
             response.headers[key] = value
-            print(request.headers)
+
 
         # --- Set-Cookie 転送 ---
         try:
@@ -84,7 +102,11 @@ async def proxy(request: Request, url: str = None):
         except Exception:
             pass
 
+
         return response
+
 
     except Exception as e:
         return PlainTextResponse("Proxy error: " + str(e), status_code=500)
+
+
